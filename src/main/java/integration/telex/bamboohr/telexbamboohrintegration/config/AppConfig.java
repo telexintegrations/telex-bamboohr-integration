7c1be5e9cd45cb6a5b86eaaf92d31dd442128214
aniebietafia <aniebietafia87@gmail.com>
2025-03-06 17:40:08 +0100

    feat: implement bamboohr service.

--- src/main/java/integration/telex/bamboohr/telexbamboohrintegration/config/AppConfig.java
+++ src/main/java/integration/telex/bamboohr/telexbamboohrintegration/config/AppConfig.java
@@ -1,13 +1,26 @@
 package integration.telex.bamboohr.telexbamboohrintegration.config;
 
-import org.springframework.context.annotation.Bean;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    @Getter
+    @Value("${bamboohr.api.key}")
+    private String bamboohrApiKey;
+
+    @Value("${bamboohr.domain}")
+    private String bamboohrCompanyName;
+
+    @Getter
+    @Value("${telex.bamboohr.webhook.url}")
+    private String telexWebhookUrl;
+
+    @Value("${bamboohr.webhook.url}")
+    private String bamboohrWebhookUrl;
+
+    public String getBamboohrWebhookUrl() {
+        return bamboohrWebhookUrl + bamboohrCompanyName + "/v1/webhooks";
     }
 }
---
+++ src/main/java/integration/telex/bamboohr/telexbamboohrintegration/service/BambooHRWebhookSetupService.java
@@ -0,0 +1,52 @@
+package integration.telex.bamboohr.telexbamboohrintegration.service;
+
+import integration.telex.bamboohr.telexbamboohrintegration.config.AppConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+@RequiredArgsConstructor
+public class BambooHRWebhookSetupService {
+    private static AppConfig appConfig;
+
+    public void createWebhook() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String webhookPayload = """
+                {
+                    "name": "New Employee Webhook",
+                    "monitorFields": ["employeeNumber", "firstName"],
+                    "postFields": {
+                        "employeeId": "Employee #",
+                        "firstName": "First Name",
+                        "lastName": "Last Name",
+                        "17": "Job Title",
+                        "acaStatus": "Employment Status: ACA Full-Time"
+                    },
+                    "url": "https://heorkuapp.com/telex-webhook",
+                    "format": "json",
+                    "frequency": {
+                        "minute": 1
+                    },
+                    "limit": {
+                        "times": 5,
+                        "seconds": 1200
+                    }
+                }
+                """;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth(appConfig.getBamboohrApiKey(), "x");
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> request = new HttpEntity<>(webhookPayload, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity(appConfig.getBamboohrWebhookUrl(), request, String.class);
+
+        System.out.println("Response: " + response.getBody());
+    }
+}
